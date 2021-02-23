7af04902a4d89d0e71cf54e0f3f8f26fe6f106f0
Brandon Knox <bknox.dev@gmail.com>
2021-02-23 16:13:12 -0700

    Initial Commit

---
+++ UnitConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConvert
+//
+//  Created by Brandon Knox on 2/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitConvert/UnitConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertApp.swift
+//  UnitConvert
+//
+//  Created by Brandon Knox on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
