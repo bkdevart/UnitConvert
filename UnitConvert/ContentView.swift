58fdf0e1a66f9c99022b0798599714460a9409c5
Brandon Knox <bknox.dev@gmail.com>
2021-02-23 17:00:35 -0700

    Added second selector for output conversion unit, input value text entry, and display for output value.  Temporary value for conversion entered until proper logic added.

--- UnitConvert/ContentView.swift
+++ UnitConvert/ContentView.swift
@@ -14,9 +14,19 @@ struct ContentView: View {
     
     let volumeUnits = ["milliliters", "liters", "cups", "pints", "gallons"]
     
+    var convertedValue: Double {
+        // TODO do calculations
+        let value = 2.2  // placeholder until real calc in
+        return value
+    }
+    
     var body: some View {
         NavigationView {
             Form {
+                Section(header: Text("Input Value")) {
+                    TextField("Value", text: $inputNumber)
+                }
+                
                 Section(header: Text("Input Unit")) {
                     Picker("Input Unit", selection: $inputUnit) {
                         ForEach(0 ..< volumeUnits.count) {
@@ -24,6 +34,19 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Output Unit")) {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(0 ..< volumeUnits.count) {
+                            Text("\(self.volumeUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Converted Value")) {
+                    Text("$\(convertedValue, specifier: "%.2f")")
                 }
             }
             .navigationBarTitle("UnitConvert")
